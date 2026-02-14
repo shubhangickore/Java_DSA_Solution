ace996a55f94b548cbbe52ebd7d6879801bdc808
Shubhangi kore <146628896+shubhangickore@users.noreply.github.com>
2026-02-14 23:23:25 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,6 +1,7 @@
 public class Palindrome {
     public static boolean isPalindrome(String str) {
-        str = str.replaceAll("\\s+", "").toLowerCase();
+        str = str.replaceAll("\\s+", "").toLowerCase(); // modifies the original string stored in the variable str by first removing
+                                                           all whitespace characters and then converting all remaining characters to lowercase. 
         int left = 0, right = str.length() - 1;
 
         while (left < right) {
