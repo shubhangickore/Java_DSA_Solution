e11454a3a911b4215b01ef35bcf9af57002555a1
shubhangickore <koreshubhangi291@gmail.com>
2026-03-20 12:52:03 +0530

    Toggle bulb problem solved

---
+++ BulbToggle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class BulbToggle {
+    public static void main(String args[]) {
+        int n = 20;
+
+        int result = (int) Math.sqrt(n);
+        System.out.println(" result is :" + result);
+    }
+
+}
